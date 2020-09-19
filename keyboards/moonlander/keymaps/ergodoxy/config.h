cd0c88f62f92370fbea6b804d9d88fe9b95ac85a
Nishant Arora <nishant.arora@appdirect.com>
2020-09-19 15:36:42 -0600

    keyboards/moonlander/keymaps/ergodoxy/config: fix

--- keyboards/moonlander/keymaps/ergodoxy/config.h
+++ keyboards/moonlander/keymaps/ergodoxy/config.h
@@ -3,5 +3,5 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define ORYX_CONFIGURATOR
-#define FIRMWARE_VERSION u8"vK6xr/qYnYz"
+#define FIRMWARE_VERSION u8"vK6xr/6w4Vr"
 #define RGB_MATRIX_STARTUP_SPD 60
