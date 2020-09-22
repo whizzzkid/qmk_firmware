e99b4c42d4ef7acf977a8e0318eceb1ab8d37d16
Nishant Arora <nishant.arora@appdirect.com>
2020-09-22 12:34:02 -0600

    new LT

--- keyboards/moonlander/keymaps/ergodoxy/config.h
+++ keyboards/moonlander/keymaps/ergodoxy/config.h
@@ -3,5 +3,5 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define ORYX_CONFIGURATOR
-#define FIRMWARE_VERSION u8"vK6xr/Na4N6"
+#define FIRMWARE_VERSION u8"vK6xr/A0bd0"
 #define RGB_MATRIX_STARTUP_SPD 60
